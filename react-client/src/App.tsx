d81423a3d0dd81d6039fb500347d11748635d5cf
byarustev <byarus45@gmail.com>
2021-08-04 03:24:25 +0300

    add material ui

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import PeopleList from './Components/PeopleList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (): JSX.Element => (
+      <Router>
+      <Container maxWidth="md">
+        <Switch>
+          <Route path="/">
+            <PeopleList />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
+);
 
 export default App;
